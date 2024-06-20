6c17c633a38ad86daaaf436f6aa5d1826733b365
Doug Hirlinger <doughirlinger@gmail.com>
2024-06-20 09:47:10 -0400

    part 1 assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,10 +3,10 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = 'What trumpet player from E. St. Louis attended The Julliard School and had a profound impact onAmerican music? ';
+let correctAnswer = 'Miles Davis';
 let candidateAnswer;
 
 
@@ -18,19 +18,27 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question('Enter candidate\'s name: ');
+  
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+if (candidateAnswer === correctAnswer) {
+  console.log('Your anwser is correct!')
+}  else {
+  console.log('Wrong anwser. Be sure to check your capitalization.')
+}
+console.log(`Your Answer: ${candidateAnswer}`);
+console.log(`Correct Answer: ${correctAnswer}`);
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +50,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
