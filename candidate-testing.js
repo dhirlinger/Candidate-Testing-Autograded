ad8bf93ab0e4428e39b5ff63fa9f92013d8e9fd7
Doug Hirlinger <doughirlinger@gmail.com>
2024-06-20 09:59:11 -0400

    assignment 1 part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,12 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName; // I didn't assign here since doing results in a prompt and the askForName func requires the same thing. 
-
+let candidateName = 'Doug Hirlinger';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'What trumpet player from E. St. Louis attended The Julliard School and had a profound impact onAmerican music? ';
-let correctAnswer = 'Miles Davis';
-let candidateAnswer; //same thing here concerning assignment and prompt requirement in askQuestion()
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = 'sally ride';
 
 
 //TODO: Variables for Part 2
